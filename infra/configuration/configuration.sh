0d4ace57c4e5963327b95d7f1a8a9e43b087d8a3
Pascal Naber <pnaber@xpirit.com>
2020-03-05 22:39:25 +0100

    change logging in scripts

--- infra/configuration/configuration.sh
+++ infra/configuration/configuration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xe
+set -e
 
 # Read configfile
 . ../$1
@@ -7,15 +7,15 @@ set -xe
 STORAGEACCOUNT_DATA_CONNECTIONSTRING=$(az storage account show-connection-string -n $STORAGEACCOUNT_DATA_NAME -g $RESOURCEGROUP_DATA --query connectionString -o tsv)
 
 az functionapp config appsettings set -n $FUNCTIONAPP_NAME  -g $RESOURCEGROUP_FUNCTIONAPP --settings \
-"tenantId=$tenantId" \
-"subscriptionId=$subscriptionId" \
-"applicationId=$applicationId" \
-"servicePrincipalPassword=$servicePrincipalPassword" \
-"tableStorageName=$DATA_TABLENAME" \
-"tableStorageConnectionString=$STORAGEACCOUNT_DATA_CONNECTIONSTRING" \
-"twitterApiKey=$twitterApiKey" \
-"twitterApiSecretKey=$twitterApiSecretKey" \
-"twitterAccessToken=$twitterAccessToken" \
-"twitterAccessTokenSecret=$twitterAccessTokenSecret" \
-"toggle_SendNotification=$TOGGLE_SENDNOTIFICATIONS"
+    "tenantId=$tenantId" \
+    "subscriptionId=$subscriptionId" \
+    "applicationId=$applicationId" \
+    "servicePrincipalPassword=$servicePrincipalPassword" \
+    "tableStorageName=$DATA_TABLENAME" \
+    "tableStorageConnectionString=$STORAGEACCOUNT_DATA_CONNECTIONSTRING" \
+    "twitterApiKey=$twitterApiKey" \
+    "twitterApiSecretKey=$twitterApiSecretKey" \
+    "twitterAccessToken=$twitterAccessToken" \
+    "twitterAccessTokenSecret=$twitterAccessTokenSecret" \
+    "toggle_SendNotification=$TOGGLE_SENDNOTIFICATIONS" &>/dev/null
 
