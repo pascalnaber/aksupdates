34c4a7d7eb6b20f0fb2ec14dfc317b3c75299f49
Pascal Naber <pnaber@xpirit.com>
2020-03-05 22:20:03 +0100

    pass secrets to bash file

--- infra/configuration/configuration.sh
+++ infra/configuration/configuration.sh
@@ -7,15 +7,15 @@ set -xe
 STORAGEACCOUNT_DATA_CONNECTIONSTRING=$(az storage account show-connection-string -n $STORAGEACCOUNT_DATA_NAME -g $RESOURCEGROUP_DATA --query connectionString -o tsv)
 
 az functionapp config appsettings set -n $FUNCTIONAPP_NAME  -g $RESOURCEGROUP_FUNCTIONAPP --settings \
-"tenantId=${{ secrets.tenantId }}" \
-"subscriptionId=${{ secrets.subscriptionId }}" \
-"applicationId=${{ secrets.applicationId }}" \
-"servicePrincipalPassword=${{ secrets.servicePrincipalPassword }}" \
+"tenantId=$tenantId" \
+"subscriptionId=$subscriptionId" \
+"applicationId=$applicationId" \
+"servicePrincipalPassword=$servicePrincipalPassword" \
 "tableStorageName=$DATA_TABLENAME" \
 "tableStorageConnectionString=$STORAGEACCOUNT_DATA_CONNECTIONSTRING" \
-"twitterApiKey=${{ secrets.twitterApiKey }}" \
-"twitterApiSecretKey=${{ secrets.twitterApiSecretKey }}" \
-"twitterAccessToken=${{ secrets.twitterAccessToken }}" \
-"twitterAccessTokenSecret=${{ secrets.twitterAccessTokenSecret }}" \
+"twitterApiKey=$twitterApiKey" \
+"twitterApiSecretKey=$twitterApiSecretKey" \
+"twitterAccessToken=$twitterAccessToken" \
+"twitterAccessTokenSecret=$twitterAccessTokenSecret" \
 "toggle_SendNotification=$TOGGLE_SENDNOTIFICATIONS"
 
