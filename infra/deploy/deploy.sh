9528b7ab8c6cecc2558ffcb33ef9e94ae28d919d
Pascal Naber <pnaber@xpirit.com>
2020-04-16 13:54:37 +0200

    just to trigger the action again

--- infra/deploy/deploy.sh
+++ infra/deploy/deploy.sh
@@ -9,7 +9,7 @@ az account set --subscription $SUBSCRIPTION_ID
 az functionapp deployment source config-zip -g $RESOURCEGROUP_FUNCTIONAPP -n $FUNCTIONAPP_NAME --src $ZIPPATH
 
 STORAGEACCOUNT_DATA_CONNECTIONSTRING=$(az storage account show-connection-string -n $STORAGEACCOUNT_DATA_NAME -g $RESOURCEGROUP_DATA --query connectionString -o tsv)
-
+ 
 az functionapp config appsettings set -n $FUNCTIONAPP_NAME  -g $RESOURCEGROUP_FUNCTIONAPP --settings \
     "tenantId=$tenantId" \
     "subscriptionId=$subscriptionId" \
